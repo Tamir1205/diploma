b400d35f02600df6d98106a032d6f33c35224934
Tamirlan Shiderbayev <TShiderbayev@dar.io>
2024-04-27 01:47:53 +0500

    Minor fix

--- src/main/java/com/example/flatsharing/user/controller/UserController.java
+++ src/main/java/com/example/flatsharing/user/controller/UserController.java
@@ -7,6 +7,8 @@ import com.example.flatsharing.user.domain.dto.UserDTO;
 import com.example.flatsharing.user.domain.mapper.UserMapper;
 import com.example.flatsharing.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,17 +21,40 @@ public class UserController {
     private UserMapper userMapper;
 
     @PostMapping("/register")
-    public String createUser(@RequestBody CreateUserDTO dto) {
-        return userService.create(dto);
+    public ResponseEntity<?> createUser(@RequestBody CreateUserDTO dto) {
+        try {
+            return ResponseEntity.ok(userService.create(dto));
+        } catch (Exception e) {
+            ErrorDetails errorDetails = new ErrorDetails("Error occurred while creating user");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDetails);
+        }
     }
 
     @PostMapping("/login")
-    public String createUser(@RequestBody AuthenticateRequestDTO dto) {
-        return userService.authenticate(dto);
+    public ResponseEntity<?> createUser(@RequestBody AuthenticateRequestDTO dto) {
+        try {
+            return ResponseEntity.ok(userService.authenticate(dto));
+        } catch (Exception e) {
+            ErrorDetails errorDetails = new ErrorDetails("Error occurred while login user");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDetails);
+        }
     }
 
     @PutMapping("/users/{id}")
-    public UserDTO updateUser(@RequestBody UpdateUserDTO dto, @PathVariable String id) {
-        return userMapper.toDTO(userService.update(dto, id));
+    public ResponseEntity<? extends Object> updateUser(@RequestBody UpdateUserDTO dto, @PathVariable String id) {
+        try {
+            return ResponseEntity.ok(userService.update(dto, id));
+        } catch (Exception e) {
+            ErrorDetails errorDetails = new ErrorDetails("Error occurred while updating user");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDetails);
+        }
+    }
+
+    public class ErrorDetails {
+        private String message;
+
+        public ErrorDetails(String message) {
+            this.message = message;
+        }
     }
 }
